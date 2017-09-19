7f8e3ef6796c480dea0066ac532550227056f13c
Kaue Pereira <kauefontes@outlook.com>
2017-09-18 20:49:12 -0400

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  myColor = "secondary"
+  colors =["primary","secondary","danger","light","dark","my-special-color"]
+  color = 0;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private changeDetectorRef: ChangeDetectorRef) {}
 
+  changeColor(){
+    this.myColor = this.colors[this.color];
+    this.changeDetectorRef.detectChanges();
+    console.log(this.colors[this.color]);
+    this.color++;
+    if (this.color > 5){
+      this.color = 0;
+    }
   }
 
 }
